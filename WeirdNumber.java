74970f357856c2fb8f141c8735f4ff846ed5bebc
Elena <hueycharn.lee@gmail.com>
2017-04-06 00:15:58 +0800

    Solution for WeirdNumber

---
+++ WeirdNumber.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner (System.in);
+        int num = sc.nextInt();
+        if (num % 2 == 1){
+            System.out.println("Weird");
+        }
+        else if (num % 2 == 0){
+            if (num >=2 && num <=5){
+                System.out.println("Not Weird");
+            }
+            else if(num >= 6 && num <=20){
+                System.out.println("Weird");
+            }
+            else if(num >20){
+                System.out.println("Not Weird");
+            }
+        }
+    }
+}
