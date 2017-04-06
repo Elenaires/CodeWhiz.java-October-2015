7a61f6fc54f60ee1a51a5d4eae4989259bad577f
Vincent Gim <vgim@sateva.com.au>
2017-04-06 09:22:05 +0800

    Cleaned up comments

--- CodeWhiz.java October 2015/WeirdNumber.java
+++ CodeWhiz.java October 2015/WeirdNumber.java
@@ -6,8 +6,7 @@ import java.util.regex.*;
 
 public class Solution {
 
-    public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    public static void main(String[] args) {        
         Scanner sc = new Scanner (System.in);
         int num = sc.nextInt();
         if (num % 2 == 1){
